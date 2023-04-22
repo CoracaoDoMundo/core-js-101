f54701b3832f64d71672915231b4deeaea68956a
Anna Coração <coracao@Annas-MacBook-Air.local>
2023-04-22 22:38:18 +0300

    feat: add solutions for 4 out of 5 tasks in date block

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -6,7 +6,6 @@
  *                                                                                           *
  ******************************************************************************************* */
 
-
 /**
  * Parses a rfc2822 string date representation into date value
  * For rfc2822 date specification refer to : http://tools.ietf.org/html/rfc2822#page-14
@@ -19,8 +18,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,10 +33,9 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
-
 
 /**
  * Returns true if specified date is leap year and false otherwise
@@ -53,10 +51,16 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
-}
+function isLeapYear(date) {
+  let d = new Date(date);
+  let year = d.getFullYear();
 
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /**
  * Returns the string representation of the timespan between two dates.
@@ -73,10 +77,142 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
-}
+function timeSpanToString(startDate, endDate) {
+  // throw new Error('Not implemented');
+  let sDate = new Date(startDate),
+    eDate = new Date(endDate);
+  // console.log("sDate:", sDate);
+  // console.log("eDate:", eDate);
+  // let sDateSeconds = Date.parse(sDate);
+  // let eDateSeconds = Date.parse(eDate);
+  // console.log("sDateSeconds:", sDateSeconds);
+  // console.log("eDateSeconds:", eDateSeconds);
 
+  let difference = eDate - sDate;
+  // console.log('difference:', difference);
+
+  let differenceDate = new Date(difference);
+  // console.log("differenceDate:", differenceDate);
+
+  let differenceIso = differenceDate.toISOString();
+  // console.log("iso:", differenceIso);
+
+  return differenceIso.slice(11, -1);
+  // let hours = difference / 3600000;
+  // console.log('hours:', hours);
+  // let hoursFin, minutes, minutesFin, seconds, secondsFin;
+
+  // if (difference === 0) {
+  //   return `00:00:00.000`;
+  // }
+
+  // if (Number.isInteger(hours)) {
+  //   if (hours >= 1 && hours < 10) {
+  //     hoursFin = `0${hours}`;
+  //   } else if (hours > 10) {
+  //     hoursFin = hours;
+  //   }
+  //   return `${hoursFin}:00:00.000`;
+  // } else {
+  //   // hours = hours.toFixed(2);
+  //   // console.log('hours:', hours);
+
+  //   if (hours < 1) {
+  //     hoursFin = '00';
+  //   } else if (hours < 10) {
+  //     hoursFin = `0${hours}`;
+  //   } else {
+  //     hoursFin = Math.trunc(hours);
+  //   }
+  //   console.log('hoursFin:', hoursFin);
+
+  //   minutes = (hours - Math.trunc(hours)) * 60;
+  //   console.log('minutes:', minutes);
+
+  //   if (Number.isInteger(minutes)) {
+  //     if (minutes > 1 && minutes < 10) {
+  //       minutesFin = `0${minutes}`;
+  //     } else if (minutes > 10) {
+  //       minutesFin = minutes;
+  //     }
+  //     return `${hoursFin}:${minutesFin}:00.000`;
+  //   } else {
+  //     // minutes = minutes.toFixed(2);
+  //     // console.log('minutes:', minutes);
+
+  //     if (minutes < 1) {
+  //       minutesFin = '00';
+  //     } else if (minutes < 10) {
+  //       minutesFin = `0${minutes}`;
+  //     } else {
+  //       minutesFin = Math.trunc(minutes);
+  //     }
+  //     console.log('minutesFin:', minutesFin);
+
+  //     seconds = ((minutes - Math.trunc(minutes)) * 60).toFixed(3);
+  //     console.log('seconds:', seconds);
+
+  //       if (seconds > 1 && seconds < 10) {
+  //         secondsFin = `0${seconds}`;
+  //       } else if (seconds > 10) {
+  //         secondsFin = seconds;
+  //       }
+  //       return `${hoursFin}:${minutesFin}:${secondsFin}`;
+  //   }
+  // }
+}
+// let differenceHours = Math.floor(eDateSeconds - sDateSeconds % 3600000);
+// let differenceMinutes = Math.floor(eDateSeconds - sDateSeconds % 600000)
+// console.log("hours:", differenceHours);
+// console.log("minutes:", differenceMinutes);
+
+// let hours = (difference / 3600000);
+// console.log('hours:', hours);
+// let hoursFin;
+
+// if (hours < 1) {
+//   hoursFin = '00';
+// } else if (hours < 10) {
+//   let hoursRound = Number(hours).toFixed();
+//   hoursFin = `0${hoursRound}`;
+// }
+// console.log('hoursFin:', hoursFin);
+
+// let minutes = (difference - hours * 3600000);
+// console.log('minutes:', minutes);
+// let minutesFin;
+// if (!Number.isInteger(hours)) {
+//   let hoursRound = Number(hours).toFixed();
+//   minutesFin = hoursRound * 60 + minutes;
+// } else if (minutes < 1) {
+//   minutesFin = '00';
+// } else if (minutes < 10) {
+//   minutesFin = `0${minutes}`;
+// } else {
+//   minutesFin = minutes;
+// }
+
+// console.log('minutesFin:', minutesFin);
+
+// let seconds = (difference - minutes * 60000 - hours * 3600000) / 1000;
+// if (seconds < 10) {
+//   seconds = `0${seconds}`;
+// }
+// console.log('seconds:', seconds);
+
+// let milseconds =
+//   difference - minutes * 60000 - hours * 3600000 - seconds * 1000;
+// console.log('milseconds:', milseconds);
+// if (milseconds === 0) {
+//   milseconds = '000';
+// } else if (milseconds < 10) {
+//   milseconds = `00${milseconds}`;
+// } else if (milseconds < 100) {
+//   milseconds = `0${milseconds}`;
+// }
+
+// return `${hoursFin}:${minutesFin}:${seconds}.${milseconds}`;
+// }
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock
@@ -94,10 +230,9 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
+function angleBetweenClockHands(date) {
   throw new Error('Not implemented');
 }
-
 
 module.exports = {
   parseDataFromRfc2822,
