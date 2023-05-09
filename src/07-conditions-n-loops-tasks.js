ed3fb9460948f1a2129181961414db270a712cdb
Anna Coração <coracao@Annas-MacBook-Air.local>
2023-05-09 18:09:25 +0300

    feat: isTriangle solution in conditions-n-loops-tasks

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -93,8 +93,12 @@ function getSumBetweenNumbers(n1, n2) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  // throw new Error('Not implemented');
+  if ((a === b && b === c) || (a < b + c && b < a + c && c < a + b)) {
+    return true;
+  }
+  return false;
 }
 
 /**
